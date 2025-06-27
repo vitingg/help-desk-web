c136187aedea30a4f42135a8dafd3b0996d81102
vitingg <victorgabel99@gmail.com>
2025-06-27 15:35:08 -0300

    feat: fixing how the selected component works

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -2,9 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "../shared/styles/index.css";
 import { App } from "./App.tsx";
+import { Select  } from "../shared/components/select.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Select />
   </StrictMode>
 );
--- src/shared/components/select.tsx
+++ src/shared/components/select.tsx
@@ -1,7 +1,27 @@
-export function Select(){
+type SelectProps = {
+  label?: string;
+};
+
+export function Select({ label }: SelectProps) {
   return (
-    <div>
-      select
-    </div>
-  )
+    <fieldset className="flex flex-col text-gray-300 focus:text-blue-base ">
+      <label htmlFor="LABEL" className=" text-2xs font-bold">
+        LABEL
+      </label>
+
+      <select
+        id="LABEL"
+        className="border-b-1 focus:outline-0 text-gray-400 focus:text-blue-base"
+        defaultValue={""}
+        required
+      >
+        <option value={""} className="font-bold text-2xs" disabled hidden>
+          OPÇÕES
+        </option>
+        <option value="OptionOne">Option one</option>
+        <option value="OptionTwo">Option two</option>
+        <option value="OptionThree">Option three</option>
+      </select>
+    </fieldset>
+  );
 }
